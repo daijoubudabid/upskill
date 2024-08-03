ab8e2ef444328990112ed091e7e64330489f5ef6
Dabid Marma <dabidmarma@gmail.com>
2024-08-03 19:41:04 +0600

    add

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "600px",
+          },
+          "@screen md": {
+            maxWidth: "720px",
+          },
+          "@screen lg": {
+            maxWidth: "960px",
+          },
+          "@screen xl": {
+            maxWidth: "1140px",
+          },
+          "@screen 2xl": {
+            maxWidth: "1320px",
+          },
+        },
+      });
+    },
+  ],
+};
